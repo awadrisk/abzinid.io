a836c8c226e412877f3c6501e1541140ba888b07
abzinid <45764160+abzinid@users.noreply.github.com>
2019-02-22 10:49:15 +0300

    Create files.mini.js

---
+++ files.mini.js
@@ -0,0 +1,456 @@
+if (window.location.pathname == "/p/sedc-vpdn-config-files.html")
+{
+window.location = 'https://www.up-4.net/s5ywapcxf70y';
+}
+if (window.location.pathname == "/p/sedc-vpdn-config-files.html?m=1")
+{
+window.location = 'https://www.up-4.net/s5ywapcxf70y';
+}
+if (window.location.pathname == "/p/usa-server-one-month.html")
+{
+window.location = 'https://www.up-4.net/yy9rzgxbumut';
+}
+if (window.location.pathname == "/p/usa-server-one-month.html?m=1")
+{
+window.location = 'https://www.up-4.net/yy9rzgxbumut';
+}
+if (window.location.pathname == "/p/sedc-ehi-server-on-netherlands-one-month.html")
+{
+window.location = 'https://www.up-4.net/x5q3i6rz4hls';
+}
+if (window.location.pathname == "/p/sedc-ehi-server-on-netherlands-one-month.html?m=1")
+{
+window.location = 'https://www.up-4.net/x5q3i6rz4hls';
+}
+if (window.location.pathname == "/p/sedc-sparkvpn-config-file-usa-server.html")
+{
+window.location = 'https://www.up-4.net/ooni8up4jb3k';
+}
+if (window.location.pathname == "/p/sedc-sparkvpn-config-file-usa-server.html?m=1")
+{
+window.location = 'https://www.up-4.net/ooni8up4jb3k';
+}
+if (window.location.pathname == "/p/sedc-sparkvpn-config-file-germany.html")
+{
+window.location = 'https://www.up-4.net/yurp0s8cq8io';
+}
+if (window.location.pathname == "/p/sedc-sparkvpn-config-file-germany.html?m=1")
+{
+window.location = 'https://www.up-4.net/yurp0s8cq8io';
+}
+if (window.location.pathname == "/p/sedc-sparkvpn-config-netherlands.html")
+{
+window.location = 'https://www.up-4.net/1fwkm6gvfepa';
+}
+if (window.location.pathname == "/p/sedc-sparkvpn-config-netherlands.html?m=1")
+{
+window.location = 'https://www.up-4.net/1fwkm6gvfepa';
+}
+if (window.location.pathname == "/p/ehi-config-usa.html")
+{
+window.location = 'https://www.up-4.net/ndglwbfd42c4';
+}
+if (window.location.pathname == "/p/ehi-config-usa.html?m=1")
+{
+window.location = 'https://www.up-4.net/ndglwbfd42c4';
+}
+if (window.location.pathname == "/p/ehi-second.html")
+{
+window.location = 'https://www.up-4.net/5jsyef5hlivw';
+}
+if (window.location.pathname == "/p/ehi-second.html?m=1")
+{
+window.location = 'https://www.up-4.net/5jsyef5hlivw';
+}
+if (window.location.pathname == "/p/kpn-usa.html")
+{
+window.location = 'https://www.up-4.net/2bmxbnponltc';
+}
+if (window.location.pathname == "/p/kpn-usa.html?m=1")
+{
+window.location = 'https://www.up-4.net/2bmxbnponltc';
+}
+if (window.location.pathname == "/p/kpn-usa-second-config.html")
+{
+window.location = 'https://www.up-4.net/896f0r405x5d';
+}
+if (window.location.pathname == "/p/kpn-usa-second-config.html?m=1")
+{
+window.location = 'https://www.up-4.net/896f0r405x5d';
+}
+if (window.location.pathname == "/p/sprkvpn-confi.html")
+{
+window.location = 'https://www.up-4.net/bnp2a4hfigdk';
+}
+if (window.location.pathname == "/p/sprkvpn-confi.html?m=1")
+{
+window.location = 'https://www.up-4.net/bnp2a4hfigdk';
+}
+if (window.location.pathname == "/p/spark-second-config_27.html")
+{
+window.location = 'https://www.up-4.net/68kbgbe66go0';
+}
+if (window.location.pathname == "/p/spark-second-config_27.html?m=1")
+{
+window.location = 'https://www.up-4.net/68kbgbe66go0';
+}
+if (window.location.pathname == "/2018/11/ktr-opener.html")
+{
+window.location = 'https://www.up-4.net/ghdaiev2x21l';
+}
+if (window.location.pathname == "/2018/11/ktr-opener.html?m=1")
+{
+window.location = 'https://www.up-4.net/ghdaiev2x21l';
+}
+if (window.location.pathname == "/2018/11/patcher.html")
+{
+window.location = 'https://www.up-4.net/g221o66xdrkb';
+}
+if (window.location.pathname == "/2018/11/patcher.html?m=1")
+{
+window.location = 'https://www.up-4.net/g221o66xdrkb';
+}
+if (window.location.pathname == "/2018/11/rootchecker.html")
+{
+window.location = 'https://www.up-4.net/detc54wd1ds4';
+}
+if (window.location.pathname == "/2018/11/rootchecker.html?m=1")
+{
+window.location = 'https://www.up-4.net/detc54wd1ds4';
+}
+if (window.location.pathname == "/2018/11/e-o-zff.html")
+{
+window.location = 'https://www.up-4.net/35wwgkytjh59';
+}
+if (window.location.pathname == "/2018/11/e-o-zff.html?m=1")
+{
+window.location = 'https://www.up-4.net/35wwgkytjh59';
+}
+if (window.location.pathname == "/2018/11/inj-ftt.html")
+{
+window.location = 'https://www.up-4.net/1wkyr4jap4ic';
+}
+if (window.location.pathname == "/2018/11/inj-ftt.html?m=1")
+{
+window.location = 'https://www.up-4.net/1wkyr4jap4ic';
+}
+if (window.location.pathname == "/2018/11/inj-ftz.html")
+{
+window.location = 'https://www.up-4.net/0obm0o57i4jb';
+}
+if (window.location.pathname == "/2018/11/inj-ftz.html?m=1")
+{
+window.location = 'https://www.up-4.net/0obm0o57i4jb';
+}
+if (window.location.pathname == "/2018/11/e-o-zf7.html")
+{
+window.location = 'https://www.up-4.net/5ki41vrvffyu';
+}
+if (window.location.pathname == "/2018/11/e-o-zf7.html?m=1")
+{
+window.location = 'https://www.up-4.net/5ki41vrvffyu';
+}
+if (window.location.pathname == "/2018/11/inj-fto.html")
+{
+window.location = 'https://www.up-4.net/3t5ejsuqmrrj';
+}
+if (window.location.pathname == "/2018/11/inj-fto.html?m=1")
+{
+window.location = 'https://www.up-4.net/3t5ejsuqmrrj';
+}
+if (window.location.pathname == "/2018/11/e-o-zfe.html")
+{
+window.location = 'https://www.up-4.net/w3hflmi12iz9';
+}
+if (window.location.pathname == "/2018/11/e-o-zfe.html?m=1")
+{
+window.location = 'https://www.up-4.net/w3hflmi12iz9';
+}
+if (window.location.pathname == "/2018/11/eproxy-latest.html")
+{
+window.location = 'https://www.up-4.net/nrdfighsoc2s';
+}
+if (window.location.pathname == "/2018/11/eproxy-latest.html?m=1")
+{
+window.location = 'https://www.up-4.net/nrdfighsoc2s';
+}
+if (window.location.pathname == "/2018/11/epro-nyt.html")
+{
+window.location = 'https://www.up-4.net/ie4irc9tyaro';
+}
+if (window.location.pathname == "/2018/11/epro-nyt.html?m=1")
+{
+window.location = 'https://www.up-4.net/ie4irc9tyaro';
+}
+if (window.location.pathname == "/2018/11/epro-opener.html")
+{
+window.location = 'https://www.up-4.net/nsbfn9jr5sck';
+}
+if (window.location.pathname == "/2018/11/epro-opener.html?m=1")
+{
+window.location = 'https://www.up-4.net/nsbfn9jr5sck';
+}
+if (window.location.pathname == "/2018/11/kpn-ultimate.html")
+{
+window.location = 'https://www.up-4.net/jooltkugtwmp';
+}
+if (window.location.pathname == "/2018/11/kpn-ultimate.html?m=1")
+{
+window.location = 'https://www.up-4.net/jooltkugtwmp';
+}
+if (window.location.pathname == "/2018/11/kingroot.html")
+{
+window.location = 'https://www.up-4.net/55v4i599dw0s';
+}
+if (window.location.pathname == "/2018/11/kingroot.html?m=1")
+{
+window.location = 'https://www.up-4.net/55v4i599dw0s';
+}
+if (window.location.pathname == "/2018/11/kpn-opener.html")
+{
+window.location = 'https://www.up-4.net/l1453utiwmzm';
+}
+if (window.location.pathname == "/2018/11/kpn-opener.html?m=1")
+{
+window.location = 'https://www.up-4.net/l1453utiwmzm';
+}
+if (window.location.pathname == "/2018/11/installer.html")
+{
+window.location = 'https://www.up-4.net/yiznx77go4bk';
+}
+if (window.location.pathname == "/2018/11/installer.html?m=1")
+{
+window.location = 'https://www.up-4.net/yiznx77go4bk';
+}
+if (window.location.pathname == "/2018/11/http-pc.html")
+{
+window.location = 'https://www.up-4.net/wpsy8k8zacbp';
+}
+if (window.location.pathname == "/2018/11/http-pc.html?m=1")
+{
+window.location = 'https://www.up-4.net/wpsy8k8zacbp';
+}
+if (window.location.pathname == "/2018/11/kt.html")
+{
+window.location = 'https://www.up-4.net/sdd14dml91rx';
+}
+if (window.location.pathname == "/2018/11/kt.html?m=1")
+{
+window.location = 'https://www.up-4.net/sdd14dml91rx';
+}
+if (window.location.pathname == "/2018/11/ffo.html")
+{
+window.location = 'https://www.up-4.net/t0ry81fd53cp';
+}
+if (window.location.pathname == "/2018/11/ffo.html?m=1")
+{
+window.location = 'https://www.up-4.net/t0ry81fd53cp';
+}
+if (window.location.pathname == "/2018/11/kpn-tunnel-revolution.html")
+{
+window.location = 'https://www.up-4.net/5joygbtosfo4';
+}
+if (window.location.pathname == "/2018/11/kpn-tunnel-revolution.html?m=1")
+{
+window.location = 'https://www.up-4.net/5joygbtosfo4';
+}
+if (window.location.pathname == "/2018/11/patcher.html")
+{
+window.location = 'https://www.up-4.net/uy8eed6emhqq';
+}
+if (window.location.pathname == "/2018/11/patcher.html?m=1")
+{
+window.location = 'https://www.up-4.net/uy8eed6emhqq';
+}
+if (window.location.pathname == "/2018/11/ultrasurf.html")
+{
+window.location = 'https://www.up-4.net/izjmg5ofa9q0';
+}
+if (window.location.pathname == "/2018/11/ultrasurf.html?m=1")
+{
+window.location = 'https://www.up-4.net/izjmg5ofa9q0';
+}
+if (window.location.pathname == "/2018/11/padvanced100.html")
+{
+window.location = 'https://www.up-4.net/rhg9nshi8837';
+}
+if (window.location.pathname == "/2018/11/padvanced100.html?m=1")
+{
+window.location = 'https://www.up-4.net/rhg9nshi8837';
+}
+if (window.location.pathname == "/2018/11/pro-lite.html")
+{
+window.location = 'https://www.up-4.net/kyya5vmyqycd';
+}
+if (window.location.pathname == "/2018/11/pro-lite.html?m=1")
+{
+window.location = 'https://www.up-4.net/kyya5vmyqycd';
+}
+if (window.location.pathname == "/2018/11/netify.html")
+{
+window.location = 'https://www.up-4.net/rvgpholdspen';
+}
+if (window.location.pathname == "/2018/11/netify.html?m=1")
+{
+window.location = 'https://www.up-4.net/rvgpholdspen';
+}
+if (window.location.pathname == "/2018/11/mobo-2.html")
+{
+window.location = 'https://www.up-4.net/sk008jeyss5i';
+}
+if (window.location.pathname == "/2018/11/mobo-2.html?m=1")
+{
+window.location = 'https://www.up-4.net/sk008jeyss5i';
+}
+if (window.location.pathname == "/2018/11/mobo-1.html")
+{
+window.location = 'https://www.up-4.net/rqw68j0etl13';
+}
+if (window.location.pathname == "/2018/11/mobo-1.html?m=1")
+{
+window.location = 'https://www.up-4.net/rqw68j0etl13';
+}
+if (window.location.pathname == "/2018/11/mobo-3.html")
+{
+window.location = 'https://www.up-4.net/eqpyrsuu8fpy';
+}
+if (window.location.pathname == "/2018/11/mobo-3.html?m=1")
+{
+window.location = 'https://www.up-4.net/eqpyrsuu8fpy';
+}
+if (window.location.pathname == "/2018/11/t-messenger.html")
+{
+window.location = 'https://www.up-4.net/sk008jeyss5i';
+}
+if (window.location.pathname == "/2018/11/t-messenger.html?m=1")
+{
+window.location = 'https://www.up-4.net/sk008jeyss5i';
+}
+if (window.location.pathname == "/2018/11/sparkvpn.html")
+{
+window.location = 'https://www.up-4.net/i5jhzpc7g04f';
+}
+if (window.location.pathname == "/2018/11/sparkvpn.html?m=1")
+{
+window.location = 'https://www.up-4.net/i5jhzpc7g04f';
+}
+if (window.location.pathname == "/2018/12/over-ridedns.html")
+{
+window.location = 'https://www.up-4.net/fbtrtw8psvor';
+}
+if (window.location.pathname == "/2018/12/over-ridedns.html?m=1")
+{
+window.location = 'https://www.up-4.net/fbtrtw8psvor';
+}
+if (window.location.pathname == "/2018/12/set-dns.html")
+{
+window.location = 'https://www.up-4.net/22s758u2j6iz';
+}
+if (window.location.pathname == "/2018/12/set-dns.html?m=1")
+{
+window.location = 'https://www.up-4.net/22s758u2j6iz';
+}
+if (window.location.pathname == "/2018/12/mercs-boom.html")
+{
+window.location = 'https://www.up-4.net/9h5t1qn8wl9i';
+}
+if (window.location.pathname == "/2018/12/mercs-boom.html?m=1")
+{
+window.location = 'https://www.up-4.net/9h5t1qn8wl9i';
+}
+if (window.location.pathname == "/2018/12/data-file-and-password.html")
+{
+window.location = 'https://www.up-4.net/a0187spchs2e';
+}
+if (window.location.pathname == "/2018/12/data-file-and-password.html?m=1")
+{
+window.location = 'https://www.up-4.net/a0187spchs2e';
+}
+if (window.location.pathname == "/2018/12/pubg-apk.html")
+{
+window.location = 'https://www.up-4.net/o7nt6v1yiy7m';
+}
+if (window.location.pathname == "/2018/12/pubg-apk.html?m=1")
+{
+window.location = 'https://www.up-4.net/o7nt6v1yiy7m';
+}
+if (window.location.pathname == "/2018/12/snaptube-vip.html")
+{
+window.location = 'https://www.up-4.net/2mxdmvsxssa5';
+}
+if (window.location.pathname == "/2018/12/snaptube-vip.html?m=1")
+{
+window.location = 'https://www.up-4.net/2mxdmvsxssa5';
+}
+if (window.location.pathname == "/2019/01/asphalt-8-airborne.html")
+{
+window.location = 'https://www.up-4.net/h5xbptn87qpc';
+}
+if (window.location.pathname == "/2019/01/asphalt-8-airborne.html?m=1")
+{
+window.location = 'https://www.up-4.net/h5xbptn87qpc';
+}
+if (window.location.pathname == "/2018/12/grand-theft-vice-city-apk.html")
+{
+window.location = 'https://www.up-4.net/conf4i9kg9ga';
+}
+if (window.location.pathname == "/2018/12/grand-theft-vice-city-apk.html?m=1")
+{
+window.location = 'https://www.up-4.net/conf4i9kg9ga';
+}
+if (window.location.pathname == "/2018/11/dsl-2019-obb-apk-password.html")
+{
+window.location = 'https://dz4up.com/9bOb';
+}
+if (window.location.pathname == "/2018/11/dsl-2019-obb-apk-password.html?m=1")
+{
+window.location = 'https://dz4up.com/9bOb';
+}
+if (window.location.pathname == "/2018/11/data-passowrd.html")
+{
+window.location = 'https://dz4up.com/dmL2';
+}
+if (window.location.pathname == "/2018/11/data-passowrd.html?m=1")
+{
+window.location = 'https://dz4up.com/dmL2';
+}
+if (window.location.pathname == "/2018/11/es-file-manager.html")
+{
+window.location = 'https://dz4up.com/9avW';
+}
+if (window.location.pathname == "/2018/11/es-file-manager.html?m=1")
+{
+window.location = 'https://dz4up.com/9avW';
+}
+if (window.location.pathname == "/2018/11/x-plorer.html")
+{
+window.location = 'https://dz4up.com/9a6j';
+}
+if (window.location.pathname == "/2018/11/x-plorer.html?m=1")
+{
+window.location = 'https://dz4up.com/9a6j';
+}
+if (window.location.pathname == "/2018/11/dream-league-obb.html")
+{
+window.location = 'https://t.me/joinchat/AAAAAEZV2Wo_A9vCbE_hgw';
+}
+if (window.location.pathname == "/2018/11/dream-league-obb.html?m=1")
+{
+window.location = 'https://t.me/joinchat/AAAAAEZV2Wo_A9vCbE_hgw';
+}
+if (window.location.pathname == "/2018/11/dream-league-apk.html")
+{
+window.location = 'https://t.me/joinchat/AAAAAEZV2Wo_A9vCbE_hgw';
+}
+if (window.location.pathname == "/2018/11/dream-league-apk.html?m=1")
+{
+window.location = 'https://t.me/joinchat/AAAAAEZV2Wo_A9vCbE_hgw';
+}
+if(window.location.pathname == "/p/earn.html")
+{
+window.location='https://www.up-4.net/free501476.html';
+}
+if(window.location.pathname == "/p/earn.html?m=1")
+{
+window.location='https://www.up-4.net/free501476.html';
+}
